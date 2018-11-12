7ddc5107b27b7a45295254e8b308fa74d7b2d199
Ah1To <Ah1to@yandex.ru>
2018-11-12 14:32:07 +0500

    Update serv.py

--- serv.py
+++ serv.py
@@ -1,8 +1,10 @@
 from http.server import HTTPServer, CGIHTTPRequestHandler
+#из модуля http.server импортирую HTTPServer и CGIHTTPRequestHandler для дальнейшего использования
 
-
-
+#создаю переменые host и port и описываю их для создания сервера и слушателя на порт
 host = 'localhost'
 port = 8080
+#создаю сервер с параметрами host и port и обработчиком событий CGIHTTPRequester
 httpd = HTTPServer((host, port), CGIHTTPRequestHandler)
+#Запускаю сервер
 httpd.serve_forever()
